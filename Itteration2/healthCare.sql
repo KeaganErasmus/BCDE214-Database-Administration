0a987a798a17b1754f08e5195d37e568276abb92
keagan erasmus <keaganerasmus12@gmail.com>
2022-08-23 21:55:45 +1200

    added carer table and carer data

--- Itteration2/healthCare.sql
+++ Itteration2/healthCare.sql
@@ -14,10 +14,24 @@ DOB date
 );
 -- Loading data into the patient table 
 SET FOREIGN_KEY_CHECKS=0;
-LOAD DATA INFILE 'C:/Temp/patient.csv' INTO TABLE Patient
+LOAD DATA LOCAL INFILE 'D:/BCDE214/BCDE214-Database-Administration/Itteration2/tables/patient.csv' INTO TABLE Patient
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
+-- Create the Carer table
+DROP TABLE IF EXISTS Carer;
+CREATE TABLE Carer
+(
+ID varchar(4) Primary Key,
+FName varchar(25),
+LName varchar(25),
+carerPhoneNum varchar(15)
+);
+
+LOAD DATA LOCAL INFILE 'D:/BCDE214/BCDE214-Database-Administration/Itteration2/tables/carer.csv' INTO TABLE Carer
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS;
 
 -- testing tables 
 select * from Patient;
+select * from Carer;
